d2b95ebd7843a809437992ded577ce170d532a3e
Payne Kerz <paynekerz@gmail.com>
2022-09-18 17:34:40 -0500

    search start

---
+++ src/components/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "c57ab5b39amsh02624b821726540p192f21jsnc14789484964",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "ce1c0e89c113fb72c977a8e940970f25"; 
