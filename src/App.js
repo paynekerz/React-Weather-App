ca214e43079c75fe7aba3d60334e87b15937f032
Payne Kerz <paynekerz@gmail.com>
2022-09-18 17:46:44 -0500

    search and yeet uneeded stuff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Search from "./components/search/search";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./components/api";
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+  
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch]).then(async (response) => {
+      const weatherResponse = await response[0].json();
+      const forcastResponse = await response[1].json();
+
+      setCurrentWeather({ city: searchData.label, ...weatherResponse });
+      setForecast({ city: searchData.label, ...forcastResponse });
+    });
+  };
   return (
     <div className="App">
+      <Search />
     </div>
   );
 }
